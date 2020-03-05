82b08fcd2d904c24e4dfac150a0a2f2138c48ace
Gabinho93 <gabin.vdml@gmail.com>
2020-03-05 10:03:28 +0100

    Part1Ex1

--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.chucknorris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jokes = JokeList.jokes;
+        for(i in jokes) {
+            Log.d("blague",i);
+        }
     }
+
+
+
 }
+
+
